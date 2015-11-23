9e0a53070187ea5955520a76722f948deb8cb34d
Greg Comstock <gcomstock@gmail.com>
2015-11-23 08:55:58 -0800

    updates slider for home page

--- front-page.php
+++ front-page.php
@@ -1,23 +1,33 @@
 <?php get_header(); ?>
 
 
+<div class="sliderDots"></div>
+
+
 <div class="frontPage">
-	<div class="frontPage__slider iosslider">
+
+
+	<div class="frontPage__slider iosSlider">
 		<div class="slider">
 			<?php
 				//you can dynamically define the category_name string based off of the url slug if you want to revise your contnt.php loop
-				$the_query = new WP_Query( array( 'category_name' => 'nature' ) );
+				$the_query = new WP_Query( array( 'category_name' => 'home' ) );
 
 				if ( $the_query->have_posts() ) {
 					while ( $the_query->have_posts() ) {
 						$the_query->the_post();
-						$img = get_the_post_thumbnail($post->ID, 'thumbnail');
+						$imgUrl = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 						$title = get_the_title();
 
+						//$img = get_the_post_thumbnail($post->ID, 'thumbnail'); //returns <img> tag, not desired here
+
+						//thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
+						//$url = $thumb['0'];
+
 						echo '
-							<div class="slide" data-title="'.$title.'">
-								'.$img.'
-							</div>
+						<div class="slide" data-title="'.$title.'">
+							<div class="slideImg" style="background-image: url('.$imgUrl.')"></div>
+						</div>
 						';
 					}
 				}
@@ -25,7 +35,7 @@
 			?>
 		</div>
 	</div>
+
+
 </div>
-
-
 <?php get_footer(); ?>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,52 @@
+function constructHomeSlider ($slider) {
+	$slider.iosSlider({
+		snapToChildren: true,
+		desktopClickDrag: true,
+		startAtSlide: 1,
+		infiniteSlider: true,
+		autoSlideTransTimer: 800,
+		autoSlide: true,
+		autoSlideTimer: 2500,
+		snapSlideCenter: true,
+		autoSlideHoverPause: false,
+		onSliderLoaded: loaded,
+		onSliderResize: resize,
+		onSlideChange: slideChange
+	});
+
+	//draw dots
+	var slideLength = $('.slide').length;
+	for (var i = 0; i < slideLength; i++) {
+		$('.sliderDots').append('<div class="dot"></div>');
+	};
+	$('.dot').eq(0).addClass('active');
+
+
+	function slideChange(args) {
+		var slideNumber = args.currentSlideNumber%slideLength;
+		$('.dot').removeClass('active');
+		$('.dot').eq(slideNumber-1).addClass('active');
+	}
+
+	function loaded(args) {
+		resize(args);
+		args.sliderContainerObject.css('opacity','1');
+	}
+
+	function resize(args) {
+		var winHeight = $(window).innerHeight();
+		args.sliderContainerObject.height(winHeight);
+	}
+}
+
+
+
+
 var $ = jQuery;
 jQuery(function($){
 
 
-	$('.frontPage__slider').iosSlider({
-		snapToChildren: true,
-		desktopClickDrag: true
-		//autoSlide: true,
-		//autoSlideTimer: 1000
-	});
-
-	console.log('init slider...')
+	constructHomeSlider($('.frontPage__slider'));
+	
 
 });
