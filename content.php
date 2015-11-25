b54779e11ee1d0cd7d887c40fa921b67ecf004c8
Greg Comstock <gcomstock@gmail.com>
2015-11-25 08:25:59 -0800

    removes masonry dependency

--- content.php
+++ content.php
@@ -1,49 +1,53 @@
-<div class="grid clearfix">
-	<div class="grid-sizer clearfix"></div><!-- /grid-sizer -->
+
+<div class="photoGrid">
+
 	<?php
-	if (have_posts()):
-		while (have_posts()): the_post(); ?>
 
-		<?php 
+		//get category slug of current page
+		global $post;
+		$slug = get_post( $post )->post_name;
 
-			global $wp_query;
-			$post_id = $wp_query->post->ID;
+		//query posts for the category $slug
+		$the_query = new WP_Query( array( 'category_name' => $slug ) );
 
-			$post = get_post( $post_id );
-			$slug = $post->post_name;
+		//loop through the posts
+		if ( $the_query->have_posts() ) {
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$imgUrl = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+				$title = get_the_title();
 
-			$args = array('category_name' => $slug);
+				//$img = get_the_post_thumbnail($post->ID, 'thumbnail'); //returns <img> tag, not desired here
 
-			if( in_category('uncategorized') ){
-				echo '<div class="grid"><br/>sorry, no content found. please make sure to select a category before publishing your posts.</div>'; 
+				$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
+				$thumbUrl = $thumb['0'];
+
+				echo '
+				<div class="photoGrid__photo" data-filepath="">
+					<div class="photoGrid__overlay">
+						<div class="photoGrid__overlay__title">'.$title.'</div>
+					</div>
+					<img src="'.$thumbUrl.'">
+				</div>
+				';
+
+				//<div class="slideImg" style="background-image: url('.$imgUrl.')"></div>
+/*
+				<div class="grid-item clearfix">
+					<div class="tabImage">
+						<?php the_post_thumbnail(); ?>
+						<div class="mobile-hover"></div><!-- /mobile-hover -->
+					</div><!-- /tabImage -->
+					<div class="excerpt">
+						<h1><?php the_title(); ?></h1>
+					</div><!-- /excerpt -->
+				</div>
+*/				
+
 			}
+		}
+		wp_reset_postdata();
 
-		?>
-
-		<?php
-		$the_query = new WP_Query( $args );
-		while ( $the_query->have_posts() ) :
-				$the_query->the_post(); ?>
-
-			<div class="grid-item clearfix">
-				<div class="tabImage">
-					<?php the_post_thumbnail(); ?>
-					<div class="mobile-hover"></div><!-- /mobile-hover -->
-				</div><!-- /tabImage -->
-				<div class="excerpt">
-					<h1><?php the_title(); ?></h1>
-				</div><!-- /excerpt -->
-			</div>
-
-		<?php
-			endwhile;
-			wp_reset_postdata();
-		?>
-				
-		<?php endwhile;
-			else: 
-				echo '<p>No Content Found</p>';
-			endif;
-		?>
+	?>
 
 </div>
